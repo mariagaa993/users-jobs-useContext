3efb3912690b6eb0759f5c3d9ff620f8ba6c9f12
Maria Alarcon <mariagaa993@gmail.com>
2020-10-20 18:11:48 -0400

    gh-pages and modify readme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/pages/Home';
 import Footer from './components/footer/Footer';
 
 const App = () => {
   	return (
     	<Fragment>
 	  		<Navbar />
+			<Home />
 			<Footer />
     	</Fragment>
   	);
