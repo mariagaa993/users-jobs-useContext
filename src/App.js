4042028853d5757d573d772431bdc01c01a187ad
Maria Alarcon <mariagaa993@gmail.com>
2020-10-21 00:04:22 -0400

    modified navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
-import ContentInit from './components/contentInit/ContentInit';
+import Home from './components/pages/Home';
+import UserPage from './components/pages/UserPage';
+import JobPage from './components/pages/JobPage';
 import Footer from './components/footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   	return (
-    	<Fragment>
-	  		<Navbar />
-			<ContentInit />
-			<Footer />
-    	</Fragment>
+		<Router>
+        	<div>
+          		<Navbar /> 
+          		<Switch>
+            		<Route exact path="/users-jobs-useContext" component={Home} />
+            		<Route path="/users" component={UserPage} />
+            		<Route path="/jobs" component={JobPage} />
+          		</Switch>
+          		<Footer />
+        	</div>
+      	</Router>
   	);
 }
 
 export default App;
+
+/* 
+	<Fragment>
+	  		<Navbar />
+			<ContentInit />
+			<Footer />
+    	</Fragment>
+*/
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import './Navbar.scss';
-import Home from '../pages/Home';
-import JobPage from '../pages/JobPage';
-import UserPage from '../pages/UserPage';
 import iconNav from './icon-nav.svg';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Home from '../pages/Home';
 
 const Navbar = () => {
     return (
-        <Router>
-          	<nav className="nav">
-          		<ul>
-				  	<li><img className="icon-nav" src={iconNav} /></li>
-            		<li><Link className="link" to="/">Home</Link></li>
-            		<li><Link className="link" to="/users">Users</Link></li>
-					<li><Link className="link" to="/jobs">Jobs</Link></li>
-          		</ul>
-        	</nav>
-        
-      		<Switch>
-      			<Route path="/" exact component={Home}></Route>
-				<Route path="/users" component={UserPage}></Route>
-          		<Route path="/jobs" component={JobPage}></Route>
-        	</Switch>
-        </Router>
+		<nav className="nav">
+			<ul>
+				<li><img className="icon-nav" src={iconNav} /></li>
+				<li style={{diaply: `translateX(${pixels}px)`}} ><Link className="link" to="/users">Users</Link></li>
+				<li><Link className="link" to="/jobs">Jobs</Link></li>
+			</ul>
+		</nav>
     );
 }
 
