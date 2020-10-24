6ff6046d41355377d6fd85aa1c8667c0c4456359
Maria Alarcon <mariagaa993@gmail.com>
2020-10-24 10:20:10 -0400

    modified navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react';
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/pages/Home';
+import JobPage from './components/pages/JobPage';
+import UserPage from './components/pages/UserPage';
 import Footer from './components/footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   	return (
-    	<Fragment>
-	  		<Navbar />	  
+		<Router>
+			<Navbar />	
+			<Switch>
+				<Route path="/" exact> <Home /> </Route>
+		  		<Route path="/users"> <UserPage /> </Route>
+				<Route path="/jobs"> <JobPage /> </Route>
+	  		</Switch>
 			<Footer />
-    	</Fragment>
+  		</Router>
   	);
 }
 
